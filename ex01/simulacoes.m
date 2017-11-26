b73c3f371d586c65b29269e3208464a9d270d50d
Gustavo Jordão <gustavo.jordao@hotmail.com.br>
2017-11-26 02:02:12 -0200

    Adicionadas informações sobre os integrantes do grupo

--- ex01/simulacoes.m
+++ ex01/simulacoes.m
@@ -9,7 +9,7 @@
 ################################################################################
 
 ## Autores:
-##    Daniel ... (...)
+##    Daniel de Aguiar Gonçalves (201222040158)
 ##    Érica Yuri Yoshiwara (201212040180)
 ##    Gustavo Jordão Nunes (201212040520)
 ##    Lucas Ramon dos Santos Alves (201222040115)
