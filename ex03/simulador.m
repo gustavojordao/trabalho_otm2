6373b167923801b97be0b0530b2eaa475fbbf9b7
lucasramon01 <lucasramonsa@gmail.com>
2017-11-26 14:48:26 -0200

    Realizando as simulações 100 vezes.

--- ex03/simulador.m
+++ ex03/simulador.m
@@ -14,28 +14,34 @@
 
 function x = simulador()
   
-  [fila1, total1] = fila(0.3, 0.25, 1, 1000)
-  
-  [fila2, total2] = fila(0.3, 0.25, 2, 1000)
-  
-  [fila3, total3] = fila(0.3, 0.2, 3, 1000)
+  for i = 1:100
 
-  subplot(3,1,1), plot(fila1,'r',total1,'g')
-  title ('Gráfico 1 - λ = 0,3, μ = 0,25, s = 1')
-  xlabel ('Tempo')
-  ylabel ('Clientes')
-  legend('Fila','Total')
+    [fila1, total1] = fila(0.3, 0.25, 1, 1000);
+    
+    [fila2, total2] = fila(0.3, 0.25, 2, 1000);
+    
+    [fila3, total3] = fila(0.3, 0.2, 3, 1000);
+    
+    h = figure(i);
+    subplot(3,1,1), plot(fila1,'r',total1,'g');
+    title ('Gráfico 1 - λ = 0,3, μ = 0,25, s = 1');
+    xlabel ('Tempo');
+    ylabel ('Clientes');
+    legend('Fila','Total');
+    
+    subplot(3,1,2), plot(fila2,'r',total2,'g');
+    title ('Gráfico 2 - λ = 0,3, μ = 0,25, s = 2');
+    xlabel ('Tempo');
+    ylabel ('Clientes');
+    legend('Fila','Total');
+    
+    subplot(3,1,3), plot(fila3,'r',total3,'g');
+    title ('Gráfico 3 - λ = 0,3, μ = 0,20, s = 3');
+    xlabel ('Tempo');
+    ylabel ('Clientes');
+    legend('Fila','Total');
   
-  subplot(3,1,2), plot(fila2,'r',total2,'g')
-  title ('Gráfico 2 - λ = 0,3, μ = 0,25, s = 2')
-  xlabel ('Tempo')
-  ylabel ('Clientes')
-  legend('Fila','Total')
-  
-  subplot(3,1,3), plot(fila3,'r',total3,'g')
-  title ('Gráfico 3 - λ = 0,3, μ = 0,20, s = 3')
-  xlabel ('Tempo')
-  ylabel ('Clientes');
-  legend('Fila','Total')  
+  end
+
   
 end
